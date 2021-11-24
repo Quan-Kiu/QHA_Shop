edeb46999dee888458f9a0c71521d23903b48ff2
minhhieu <hieuminhtruong2505@gmail.com>
2021-11-25 05:48:14 +0700

    Add Image Create Banner

--- lib/screens/homeScreen/components/body.dart
+++ lib/screens/homeScreen/components/body.dart
@@ -21,21 +21,31 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            child: Row(
+            child:Row(
               children: <Widget>[
-                Text('Danh Mục',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Colors.blue.shade900))
+                Image.asset('assets/images/PromotionImage.png')
               ],
+            )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left:defaultPaddingLeft),
+            child: Container(
+              child: Row(
+                children: <Widget>[
+                  Text('Danh Mục',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.blue.shade900))
+                ],
+              ),
             ),
           ),
           const SizedBox(
             height: 25,
           ),
           SizedBox(
-              height: 330,
+              height: 230,
               child: GridView.builder(
                   gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
                       maxCrossAxisExtent: 100,
@@ -46,16 +56,29 @@ class Body extends StatelessWidget {
                   itemBuilder: (BuildContext context, index) {
                     return MyButtonCategory(buttonCate: buttonCate[index]);
                   })),
+          Padding(
+            padding: const EdgeInsets.only(left:defaultPaddingLeft,right: defaultPaddingLeft),
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Giá sốc',
+                  style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.blue.shade900)),
+                  Text('Xem thêm',
+                  style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.blue))
+                ],
+                
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 }
-// GridView.count(
-//               crossAxisCount: 4,
-//               mainAxisSpacing: 16,
-//               crossAxisSpacing: 16,
-//               shrinkWrap: true,
-//               padding: const EdgeInsets.only(left: 24, right: 24),
-//               children: 
-//             ),
